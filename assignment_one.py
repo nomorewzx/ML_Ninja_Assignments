60a1b4a1245082572365a80eb0a6b46985ff3661
Wang Zhenxuan <zxwang@thoughtworks.com>
2017-03-21 20:45:06 +0800

    Finish the reset

--- assignment_one.py
+++ assignment_one.py
@@ -66,7 +66,21 @@ print(out_1c)
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+x_1d = tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+  30.97266006,  26.67541885,  38.08450317,  20.74983215,
+  34.94445419,  34.45999146,  29.06485367,  36.01657104,
+  27.88236427,  20.56035233,  30.20379066,  29.51215172,
+  33.71149445,  28.59134293,  36.05556488,  28.66994858])
+
+out_1d = tf.where(tf.greater(x_1d, 30))
+
+elements = tf.gather(x_1d, out_1d)
+
+with tf.Session() as sess:
+    out_1d = sess.run([out_1d, elements])
+    print("===========1d==============")
+    print(out_1d)
+    print(elements)
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -74,7 +88,13 @@ print(out_1c)
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+x_1e = tf.diag(tf.range(1,7,1))
+x_1e = tf.diag(tf.range(1,7,1))
+
+with tf.Session() as sess:
+    x_1e = sess.run(x_1e)
+    print("=========1e==========")
+    print(x_1e)
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -82,7 +102,14 @@ print(out_1c)
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-# YOUR CODE
+x_1f = tf.truncated_normal([10,10])
+
+deter = tf.matrix_determinant(x_1f)
+
+with tf.Session() as sess:
+    deter = sess.run(deter)
+    print("=========1f==========")
+    print(deter)
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -90,7 +117,13 @@ print(out_1c)
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x_1g = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+
+unique_array = tf.unique(x_1g)
+with tf.Session() as sess:
+    unique_array = sess.run(unique_array)
+    print("=========1g==========")
+    print(unique_array[0])
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -102,4 +135,21 @@ print(out_1c)
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE
+x_1h = tf.truncated_normal([300])
+y_1h = tf.truncated_normal([300])
+
+x_1h_sub_y_1h = tf.subtract(x_1h, y_1h)
+
+mean_x_sub_y = tf.reduce_mean(x_1h_sub_y_1h)
+square_x_sub_y = tf.square(mean_x_sub_y)
+mse = tf.reduce_mean(square_x_sub_y)
+
+sum_abs = tf.reduce_sum(tf.abs(x_1h_sub_y_1h))
+
+out_1h = tf.cond(tf.less(mean_x_sub_y, 0.0), lambda : mse, lambda : sum_abs)
+
+with tf.Session() as sess:
+    mean_x_sub_y, out_1h = sess.run([mean_x_sub_y, out_1h])
+    print("============1h=============")
+    print(mean_x_sub_y)
+    print(out_1h)
